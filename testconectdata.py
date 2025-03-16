cd87c7c22fbd72a76ae380a7ef7df3ba8c922951
Yu Hsiang (KINN) <109473697+kinncode@users.noreply.github.com>
2025-03-16 19:40:52 +0800

    Delete testconectdata.py

--- testconectdata.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine('mssql+pyodbc://@DESKTOP-DDIAQJQ\SQLEXPRESS/SROIV2?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes')
-try:
-    connection = engine.connect()
-    print("連線成功！")
-    connection.close()
-except Exception as e:
-    print(f"連線失敗：{e}")
